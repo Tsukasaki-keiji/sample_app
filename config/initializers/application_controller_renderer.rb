36082b53e33f69670f022184299cf05b7e341253
EC2 Default User <abcdquont56789@gmail.com>
2020-09-07 05:38:20 +0000

    Update シークレットキー追記

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '8f409e2f32c27ef3bd4de923eff05bbcca4fcce1f6d146390adf1aca1c1895174e541cea06af13ed894a0278d410393713a4d7bca0e58bac6300e2ec6023fe30'
